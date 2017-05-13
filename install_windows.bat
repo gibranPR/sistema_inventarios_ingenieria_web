71ab5076f2efbb9851c99c9565f88a06564cac16
Rosario Iván Román <develdkmd@outlook.com>
2017-05-12 18:20:04 -0600

    #2 Se actualizó el copiado del archivo
    El archivo AuthenticatesUsers.php era sobreescrito al ejecutar el composer install, por lo que se optó en guardarlo en una carpeta aparte y hacer el copiado de esta posteriormente a la instalación, también se actualizó el ignore y se agregaron instalables para linux y windows

---
+++ install_windows.bat
@@ -0,0 +1,7 @@
+composer install
+copy .env.example .env
+php artisan key:generate
+bower install
+copy archivos_modificados/AuthenticatesUsers.php vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
+echo "Editar el archivo .env y correr el comando:"
+echo "php artisan migrate:refresh --seed"
